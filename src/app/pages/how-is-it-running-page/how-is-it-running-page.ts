f90d94c79af710f8cea8964a4ca38884cf035b79
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-07 04:46:13 +0100

    Finished up bangalore section

--- src/app/pages/how-is-it-running-page/how-is-it-running-page.ts
+++ src/app/pages/how-is-it-running-page/how-is-it-running-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {BangaloreSection} from './sections/bangalore-section/bangalore-section';
 
 @Component({
   selector: 'bb-how-is-it-running-page',
-  imports: [],
+  imports: [
+    BangaloreSection
+  ],
   templateUrl: './how-is-it-running-page.html',
   styleUrl: './how-is-it-running-page.scss',
 })
