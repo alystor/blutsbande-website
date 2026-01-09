8969e0a2a7c7b125dd07e859fe2688a18585e7fc
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-09 16:21:30 +0100

    Manchester Section Finished

--- src/app/pages/menstruation-archive-page/menstruation-archive-page.ts
+++ src/app/pages/menstruation-archive-page/menstruation-archive-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {TokyoSection} from './sections/tokyo-section/tokyo-section';
+import {ManchesterSection} from './sections/manchester-section/manchester-section';
 
 @Component({
   selector: 'bb-menstruation-archive-page',
   imports: [
-    TokyoSection
+    TokyoSection,
+    ManchesterSection
   ],
   templateUrl: './menstruation-archive-page.html',
   styleUrl: './menstruation-archive-page.scss',
---
+++ src/app/pages/menstruation-archive-page/sections/manchester-section/manchester-section.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {TextView} from '../../../../components/text-view/text-view';
+import {TextContentPipe} from '../../../../pipes/text-content-pipe';
+import {ImageView} from '../../../../components/image-view/image-view';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+@Component({
+  selector: 'bb-manchester-section',
+  imports: [
+    TextView,
+    TextContentPipe,
+    ImageView
+  ],
+  templateUrl: './manchester-section.html',
+  styleUrl: './manchester-section.scss',
+})
+export class ManchesterSection {
+
+  currentIndex = 0
+
+  protected imageLoaded() {
+
+    const frames = [1, 2, 3, 4, 5, 6].map(n => document.getElementById("manchester-frame-" + n))
+
+    ScrollTrigger.create({
+      trigger: "#manchester-animation-container",
+      start: "top 66%",
+      end: "top 33%",
+      markers: true,
+      onUpdate: self => {
+        const imageIndex = Math.min(frames.length - 1, Math.floor(self.progress * frames.length))
+        if (imageIndex != this.currentIndex) {
+          this.currentIndex = imageIndex
+          frames.forEach((frame, index) => {
+            frame!.style.opacity = (index === this.currentIndex) ? '1' : '0'
+          })
+        }
+      }
+    })
+  }
+
+}
