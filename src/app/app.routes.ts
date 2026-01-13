97750e960b5235c097badfde5e42271dccf36445
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-13 01:20:37 +0100

    Added bibliography and imprint page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import {WhenDoesItRunPage} from './pages/when-does-it-run-page/when-does-it-run-
 import {WhyDoesItRunPage} from './pages/why-does-it-run-page/why-does-it-run-page';
 import {HowIsItRunningPage} from './pages/how-is-it-running-page/how-is-it-running-page';
 import {MenstruationArchivePage} from './pages/menstruation-archive-page/menstruation-archive-page';
+import {ImprintPage} from './pages/imprint-page/imprint-page';
+import {BibliographyPage} from './pages/bibliography-page/bibliography-page';
 
 export const routes: Routes = [
   {path: '', component: LandingPage},
   {path: 'when-does-it-run', component: WhenDoesItRunPage},
   {path: 'why-does-it-run', component: WhyDoesItRunPage},
   {path: 'how-is-it-running', component: HowIsItRunningPage},
-  {path: 'menstruation-archive', component: MenstruationArchivePage}
+  {path: 'menstruation-archive', component: MenstruationArchivePage},
+  {path: 'imprint', component: ImprintPage},
+  {path: 'bibliography', component: BibliographyPage}
 ];
---
+++ src/app/pages/imprint-page/imprint-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bb-imprint-page',
+  imports: [],
+  templateUrl: './imprint-page.html',
+  styleUrl: './imprint-page.scss',
+})
+export class ImprintPage {
+
+}
