a653191860686af04ae8e03440de31933c9f2e7f
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-16 02:41:59 +0100

    Finished Donetsk Section

--- src/app/pages/why-does-it-run-page/why-does-it-run-page.ts
+++ src/app/pages/why-does-it-run-page/why-does-it-run-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {AlexandriaSection} from './sections/alexandria-section/alexandria-section';
+import {DonetskSection} from './sections/donetsk-section/donetsk-section';
 
 @Component({
   selector: 'bb-why-does-it-run-page',
   imports: [
-    AlexandriaSection
+    AlexandriaSection,
+    DonetskSection
   ],
   templateUrl: './why-does-it-run-page.html',
   styleUrl: './why-does-it-run-page.scss',
