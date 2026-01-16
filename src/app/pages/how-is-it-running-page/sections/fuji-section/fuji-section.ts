b875585af3729d76221c02bf4ffdafd00823811e
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-16 05:51:36 +0100

    Fixed up fuji with new layout

--- src/app/pages/how-is-it-running-page/sections/fuji-section/fuji-section.ts
+++ src/app/pages/how-is-it-running-page/sections/fuji-section/fuji-section.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {ImageView} from '../../../../components/image-view/image-view';
 import {TextView} from '../../../../components/text-view/text-view';
 import {TextContentPipe} from '../../../../pipes/text-content-pipe';
@@ -19,31 +19,40 @@ gsap.registerPlugin(ScrollTrigger)
 })
 export class FujiSection {
 
-  constructor(private cdk: ChangeDetectorRef) {
-  }
-
-  images = [
-    "/fuji/E1.png",
-    "/fuji/E2.png",
-    "/fuji/E3.png",
-    "/fuji/E4.png"
-  ]
-
   currentIndex = 0
-  currentImage = this.images[this.currentIndex]
 
-  imageLoaded() {
+  protected imageLoaded() {
+
+    const frames = [1, 2, 3].map(n => document.getElementById("fuji-frame-" + n))
+
     ScrollTrigger.create({
-      trigger: "#fuji-image-1",
-      start: "top center",
-      endTrigger: "#fuji-image-1",
-      end: "bottom 30%",
+      trigger: "#fuji-animation-container",
+      start: "top 33%",
+      end: "center 40%",
       onUpdate: self => {
-        const imageIndex = Math.min(this.images.length - 1, Math.floor(self.progress * this.images.length))
+        const imageIndex = Math.min(frames.length - 1, Math.floor(self.progress * frames.length))
         if (imageIndex != this.currentIndex) {
           this.currentIndex = imageIndex
-          this.currentImage = this.images[this.currentIndex]
-          this.cdk.detectChanges()
+          frames.forEach((frame, index) => {
+            frame!.style.opacity = (index === this.currentIndex) ? '1' : '0'
+          })
+        }
+      }
+    })
+
+    ScrollTrigger.create({
+      trigger: "#fuji-transition-container",
+      start: "top 80%",
+      end: "top 1px",
+      onEnter: () => gsap.set("#fuji-transition-container", {opacity: 1, pointerEvents: "auto"}),
+      onLeave: () => gsap.set("#fuji-transition-container", {opacity: 0, pointerEvents: "none"}),
+      onEnterBack: () => gsap.set("#fuji-transition-container", {opacity: 1, pointerEvents: "auto"}),
+      onLeaveBack: () => gsap.set("#fuji-transition-container", {opacity: 0, pointerEvents: "none"}),
+      onUpdate: self => {
+        if (self.progress > 0.2 && self.progress < 0.8) {
+          gsap.to("#fuji-transition-text", {opacity: 1})
+        } else {
+          gsap.to("#fuji-transition-text", {opacity: 0})
         }
       }
     })
