50292a801635240ae431e4cdf2bcd09af891f60e
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-16 15:48:12 +0100

    Finished liverpool section

--- src/app/pages/how-is-it-running-page/how-is-it-running-page.ts
+++ src/app/pages/how-is-it-running-page/how-is-it-running-page.ts
@@ -4,6 +4,7 @@ import {FujiSection} from './sections/fuji-section/fuji-section';
 import {HamburgSection} from './sections/hamburg-section/hamburg-section';
 import {KyotoSection} from './sections/kyoto-section/kyoto-section';
 import {MadridSection} from './sections/madrid-section/madrid-section';
+import {LiverpoolSection} from './sections/liverpool-section/liverpool-section';
 
 @Component({
   selector: 'bb-how-is-it-running-page',
@@ -12,7 +13,8 @@ import {MadridSection} from './sections/madrid-section/madrid-section';
     FujiSection,
     HamburgSection,
     KyotoSection,
-    MadridSection
+    MadridSection,
+    LiverpoolSection
   ],
   templateUrl: './how-is-it-running-page.html',
   styleUrl: './how-is-it-running-page.scss',
