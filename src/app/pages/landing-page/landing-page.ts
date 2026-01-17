84eccc5c590faaf83f435956604e8e1b8398e572
Fabian B <49801250+alystor@users.noreply.github.com>
2026-01-17 13:12:30 +0100

    Proper landing page implemented

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {ImageView} from '../../components/image-view/image-view';
+import {TextView} from '../../components/text-view/text-view';
+import {TextContentPipe} from '../../pipes/text-content-pipe';
+import {NextPageButton} from '../../components/next-page-button/next-page-button';
 
 @Component({
   selector: 'bb-landing-page',
-  imports: [],
+  imports: [
+    ImageView,
+    TextView,
+    TextContentPipe,
+    NextPageButton
+  ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
