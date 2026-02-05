257e407aa09e2c153349d44ffa2454818d6e209a
Fabian B <49801250+alystor@users.noreply.github.com>
2026-02-05 17:40:37 +0100

    Finished bibliography site

--- src/app/pages/bibliography-page/bibliography-page.ts
+++ src/app/pages/bibliography-page/bibliography-page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {TextView} from "../../components/text-view/text-view";
+import {TextContentPipe} from '../../pipes/text-content-pipe';
 
 @Component({
   selector: 'bb-bibliography-page',
-  imports: [],
+  imports: [
+    TextView,
+    TextContentPipe
+  ],
   templateUrl: './bibliography-page.html',
   styleUrl: './bibliography-page.scss',
 })
