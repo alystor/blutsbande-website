a3141bca90b2821e515de3f20e08915adbb86dd8
Fabian B <49801250+alystor@users.noreply.github.com>
2026-02-06 00:41:04 +0100

    Changed to a floating scrollbar for better layouting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavigationBar} from './components/navigation-bar/navigation-bar';
+import {OverlayScrollbars} from 'overlayscrollbars';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,16 @@ import {NavigationBar} from './components/navigation-bar/navigation-bar';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('blutsbande-website');
+
+  ngOnInit() {
+    OverlayScrollbars(document.body, {
+      scrollbars: {
+        autoHide: 'leave',
+        autoHideDelay: 800,
+      }
+    });
+  }
+
 }
